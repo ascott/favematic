157178db4038d55da37aada752b935438af8810a
alanna <alanna.scott@gmail.com>
2011-11-07 21:09:51 -0800

    is _uid correct?

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ function getRandomInt(min, max) {
 }
     
 function getUserFollowings(curUserId){
+  $("#mix").html('');
   $.ajax({
     url: 'https://api.soundcloud.com/users/'+curUserId+'/followings.json?client_id=956307a721999662072e3d9978287449',
     type: 'GET',
